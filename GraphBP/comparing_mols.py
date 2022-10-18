7deb1c62382e2abc67695db78a118f845aa09e4e
LunaBaozi <luna.pianesi@gmail.com>
2022-10-18 22:25:27 +0200

    add new csv inhibitor file

--- GraphBP/comparing_mols.py
+++ GraphBP/comparing_mols.py
@@ -25,26 +25,10 @@ def addHs2molfromsmiles(smiles, filename='Hmol.sdf', output_type=None):
     #elif output_type == 'sdf'
     return  
 
-# TOZASERTIB
-# tozasertib_c_addedH = addHs2molfromsmiles(tozasertib_c, 'tozasertib_c_addedH.sdf', 'smiles')
-# print(tozasertib_c_addedH)
 
-# print(converter('tozasertib_c_addedH.sdf'))
-# tozasertib_i_addedH = converter(addHs2molfromsmiles(tozasertib_i, 'tozasertib_i_addedH.sdf', 'smiles'))
-# # HESPERADIN
-# hesperadin_c_addedH = converter(addHs2molfromsmiles(hesperadin_c, 'hesperadin_c_addedH.sdf', 'smiles'))
-# hesperadin_i_addedH = converter(addHs2molfromsmiles(hesperadin_i, 'hesperadin_i_addedH.sdf', 'smiles'))
-# # ZM447439
-# zm447439_c_addedH = converter(addHs2molfromsmiles(zm447439_c, 'zm447439_c_addedH.sdf', 'smiles'))
-# zm447439_i_addedH = converter(addHs2molfromsmiles(zm447439_i, 'zm447439_i_addedH.sdf', 'smiles'))
-
-
-
-# from rdkit import Chem
 # GREAT! THIS WORKS FOR COMPARING MOLECULES THAT HAVE DIFFERENT SMILES
 # myPattern = 'CN1CCN(CC1)c1nc(Sc2ccc(cc2)NC(=O)C2CC2)nc(c1)Nc1[nH]nc(c1)C'
 # myMolecule = '[H]c1c(N([H])c2c([H])c(C([H])([H])[H])nn2[H])nc(Sc2c([H])c([H])c(N([H])C(=O)C3([H])C([H])([H])C3([H])[H])c([H])c2[H])nc1N1C([H])([H])C([H])([H])N(C([H])([H])[H])C([H])([H])C1([H])[H]'
-
 # a = Chem.CanonSmiles(myPattern)
 # b = Chem.CanonSmiles(myMolecule)
 # print(a)
@@ -52,17 +36,37 @@ def addHs2molfromsmiles(smiles, filename='Hmol.sdf', output_type=None):
 # print(a==b)
 
 
-known_aurkb_inhibitors = pd.read_csv('/home/luna/Documents/Coding/GraphBP/GraphBP/aurkb_inhibitors.csv', header=0)
-# print(type(known_aurkb_inhibitors))  # pandas dataframe
-# known_aurkb_inhibitors.to_dict(orient='list')
-ciao = dict(zip(known_aurkb_inhibitors.inhibitor, known_aurkb_inhibitors.smiles))
-aurkb_inhibitors = copy.deepcopy(ciao)
-aurkb_inhibitors = {key:[] for key in aurkb_inhibitors}
+# known_aurkb_inhibitors = pd.read_csv('/home/luna/Documents/Coding/GraphBP/GraphBP/aurkb_inhibitors.csv', header=0)
+# # print(type(known_aurkb_inhibitors))  # pandas dataframe
+# # known_aurkb_inhibitors.to_dict(orient='list')
+# ciao = dict(zip(known_aurkb_inhibitors.inhibitor, known_aurkb_inhibitors.smiles))
+# aurkb_inhibitors = copy.deepcopy(ciao)
+# aurkb_inhibitors = {key:[] for key in aurkb_inhibitors}
+
+
+known_aurkb_inhibitors = pd.read_csv('/home/luna/Documents/Coding/GraphBP/GraphBP/aurdata/aurorakinaseBinteractions.csv', header=0)
+inhibitors_dict = dict(zip(known_aurkb_inhibitors.iloc[:, 3], known_aurkb_inhibitors.iloc[:, 7]))
+correspondence_dict = copy.deepcopy(inhibitors_dict)
+correspondence_dict = {key:[] for key in correspondence_dict}
+
+# print(inhibitors_dict)
+# print(list(known_aurkb_inhibitors.columns))
+# print(known_aurkb_inhibitors.iloc[:, 0])    # target id
+# print(known_aurkb_inhibitors.iloc[:, 1])    # target uniprot
+# print(known_aurkb_inhibitors.iloc[:, 2])    # target species
+# print(known_aurkb_inhibitors.iloc[:, 3])    # ligand
+# print(known_aurkb_inhibitors.iloc[:, 4])    # ligand id
+# print(known_aurkb_inhibitors.iloc[:, 5])    # ligand species
+# print(known_aurkb_inhibitors.iloc[:, 6])    # ligand pubchem cid
+# print(known_aurkb_inhibitors.iloc[:, 7])    # smiles
+# print(known_aurkb_inhibitors.iloc[:, 8])
+# print(known_aurkb_inhibitors.iloc[:, 9])
+# print(known_aurkb_inhibitors.iloc[:, 10])
+# print(known_aurkb_inhibitors.iloc[:, 11])
 
 
 
-
-def tanimoto_calc(smi1, smi2):   #(smi1, smi2):
+def tanimoto_calc(smi1, smi2):
     
     mol1 = Chem.MolFromSmiles(smi1)
     mol2 = Chem.MolFromSmiles(smi2)
@@ -74,17 +78,10 @@ def tanimoto_calc(smi1, smi2):   #(smi1, smi2):
     return s
 
 
-# THERE MIGHT BE SOME NONE AROUND
 
 def compare_mol_smiles():
 
     tanimoto = list()
-
-    # matches = copy.deepcopy(aurkb_inhibitors)
-    # matches = {'tozasertib_c': [],
-    #            'hesperadin_c': [],
-    #            'zm447439_c': []
-    #            }
 
     gen_mols_dir = '/home/luna/Documents/Coding/GraphBP/GraphBP/aurdata/aurkb_gen_complex'
 
@@ -93,17 +90,17 @@ def compare_mol_smiles():
         
         for filename in os.listdir(rel_path):
             if 'ligand' in filename:
-                sppl = Chem.SDMolSupplier(os.path.join(rel_path, filename)) #if os.path.isfile(filename):
+                sppl = Chem.SDMolSupplier(os.path.join(rel_path, filename)) 
                 
                 for mol in sppl:
                     if mol is not None:  # some compounds cannot be loaded.
                         basic_smiles_pattern = Chem.MolToSmiles(mol)
-                        canon_smiles_pattern = Chem.CanonSmiles(basic_smiles_pattern)
+                        # canon_smiles_pattern = Chem.CanonSmiles(basic_smiles_pattern)
                         
-                        for name, compound in ciao.items():
+                        for name, compound in inhibitors_dict.items():
                             # print(compound)
-                            basic_smiles_test = compound
-                            canon_smiles_test = Chem.CanonSmiles(compound)
+                            basic_smiles_test = str(compound)
+                            # canon_smiles_test = Chem.CanonSmiles(compound)
 
                             # should we use the standard or canonical smiles?
                             simil = tanimoto_calc(basic_smiles_pattern, basic_smiles_test)
@@ -124,14 +121,13 @@ def compare_mol_smiles():
     return tanimoto #matches
 
 
-# print(compare_mol_smiles())
-# compare_mol_smiles()
+
 
 tanimoto = compare_mol_smiles()
 
-# for tlist in tanimoto:
-#     print(*tlist)
+# # for tlist in tanimoto:
+# #     print(*tlist)
 
-# with open('output.csv', 'w', newline='') as csvfile:
-#     writer = csv.writer(csvfile)
-#     writer.writerows(tanimoto)
+with open('output_withcsvfromwebsite.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(tanimoto)
