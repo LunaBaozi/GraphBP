098364f52b6b69a1efe74e0bb748956a3801c2ff
LunaBaozi <luna.pianesi@gmail.com>
2022-10-22 11:04:36 +0200

    add 300 ligands version

--- GraphBP/createEBdataset.py
+++ GraphBP/createEBdataset.py
@@ -8,9 +8,9 @@ from rdkit import Chem
 aur_protein_pdb = '4af3A'
 rec_name = '4af3_A_rec'
 aurkb_file = '/home/luna/Documents/Coding/GraphBP/GraphBP/aurdata/AURKB_HUMAN_54_344_0/4af3_A_rec.pdb'
-gen_ligands_path = '/home/luna/Documents/Coding/GraphBP/GraphBP/aurdata/gen_mols_epoch_33/content/GraphBP/GraphBP/trained_model/gen_mols_epoch_33'
-aurkb_gen_complex = '/home/luna/Documents/Coding/GraphBP/GraphBP/aurdata/aurkb_gen_complex'
-write_dir = '/home/luna/Documents/Coding/GraphBP/GraphBP/aurdata/aurkb_gen_complex/molsHs_added'
+gen_ligands_path = '/home/luna/Documents/Coding/GraphBP/GraphBP/aurdata/gen_mols_epoch_33_300/content/GraphBP/GraphBP/trained_model/gen_mols_epoch_33/'
+aurkb_gen_complex = '/home/luna/Documents/Coding/GraphBP/GraphBP/aurdata/aurkb_gen_complex_300'
+write_dir = '/home/luna/Documents/Coding/GraphBP/GraphBP/aurdata/aurkb_gen_complex_300/molsHs_added'
 
 
 
@@ -23,8 +23,13 @@ def converter(file_name):
             smi = Chem.MolToSmiles(mol)
             name = mol.GetProp("_Name")
             out_file.write(f"{smi}\t{name}\n")
-    out_file.close()
-    return smi
+
+            out_file.close()
+            return smi
+    return None
+        # else: break
+    # out_file.close()
+    # return smi
 
 
 def addHs2molfromsmiles(smiles):  
@@ -43,7 +48,7 @@ def createEquiBind_dataset():
 
     os.makedirs(os.path.dirname(write_dir), exist_ok=True)
 
-    for i in range(1, 101):
+    for i in range(1, 301):
         os.makedirs(os.path.join(aurkb_gen_complex, f'{aur_protein_pdb}{i}'))
 
         subfolder = os.path.join(aurkb_gen_complex, f'{aur_protein_pdb}{i}')
@@ -53,11 +58,25 @@ def createEquiBind_dataset():
         shutil.copy(f'{gen_ligands_path}/{i}.sdf', subfolder)
 
         # Returns
-        mol_addedHs = addHs2molfromsmiles(converter(noH_mol))
-        shutil.move('Hmol.sdf', subfolder)
+        sppl = Chem.SDMolSupplier(noH_mol)
+        outname = noH_mol.replace(".sdf", ".txt")
+        out_file = open(outname, "w")
+        for mol in sppl:
+            if mol is not None:  # some compounds cannot be loaded.
+                smi = Chem.MolToSmiles(mol)
+                name = mol.GetProp("_Name")
+                out_file.write(f"{smi}\t{name}\n")
+            else: break
 
-        os.rename(f'{subfolder}/{rec_name}.pdb', f'{subfolder}/{aur_protein_pdb}{i}_protein.pdb')
-        os.rename(f'{subfolder}/Hmol.sdf', f'{subfolder}/{aur_protein_pdb}{i}_ligand.sdf')
+        out_file.close()
+        # return smi
+        # smi = converter(noH_mol)
+        if smi is not None:
+            mol_addedHs = addHs2molfromsmiles(smi)
+            shutil.move('Hmol.sdf', subfolder)
+
+            os.rename(f'{subfolder}/{rec_name}.pdb', f'{subfolder}/{aur_protein_pdb}{i}_protein.pdb')
+            os.rename(f'{subfolder}/Hmol.sdf', f'{subfolder}/{aur_protein_pdb}{i}_ligand.sdf')
 
         
 createEquiBind_dataset()
